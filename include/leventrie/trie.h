33f603eea3beeb563fdd175ef516937d3a607f37
Ivan Smirnov <i.s.smirnov@gmail.com>
2017-10-17 20:41:47 +0100

    Add basic min_distance() implementation

--- include/leventrie/trie.h
+++ include/leventrie/trie.h
@@ -1,6 +1,8 @@
 #ifndef LEVENTRIE_TRIE_H
 #define LEVENTRIE_TRIE_H
 
+#include "automaton.h"
+
 #include <string>
 #include <vector>
 
@@ -30,6 +32,12 @@ public:
         }
     }
 
+    int min_distance(const std::string& word, int max_edits) const {
+        Automaton aut(word, max_edits);
+        auto min_cost = min_distance_impl_(*this, aut, aut.start());
+        return min_cost > max_edits ? 0xff : min_cost;
+    }
+
     void insert(const std::string& word) noexcept {
         auto* ptr = this;
         for (auto ch : word) {
@@ -48,6 +56,19 @@ public:
             }
         }
         ptr->word_ = word;
+    }
+
+private:
+    static int min_distance_impl_(const trie& tree, const Automaton& aut, const State& state) {
+        int min_cost = tree.is_word() ? aut.cost(state) : 0xff;
+        tree.foreach([&](const trie& node, char ch) {
+            bool can_match = false;
+            auto next_state = aut.step(state, ch, &can_match);
+            if (can_match) {
+                min_cost = std::min(min_cost, min_distance_impl_(node, aut, next_state));
+            }
+        });
+        return min_cost;
     }
 };
 
