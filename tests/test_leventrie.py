18d4fdbdc473823ac1e132c03b8f6f13d644ed20
Ivan Smirnov <i.s.smirnov@gmail.com>
2017-10-17 20:42:05 +0100

    Add a basic test for min_distance()

--- tests/test_leventrie.py
+++ tests/test_leventrie.py
@@ -1,5 +1,16 @@
-import leventrie
+import pytest
+from leventrie import Trie
 
 
-def test_smoke():
-    assert leventrie.test == 1
+@pytest.fixture
+def trie():
+    return Trie(['foo', b'bar', 'baz', 'hello'])
+
+
+def test_trie(trie):
+    assert trie.min_distance('', 0) == 255
+    assert trie.min_distance('', 3) == 3
+
+    assert trie.min_distance('fo', 0) == 255
+    assert trie.min_distance('fo', 1) == 1
+
